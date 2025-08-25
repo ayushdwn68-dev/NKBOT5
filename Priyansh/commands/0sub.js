02ef167ba49384f2ebe36f7e774e3acd482f8769
NKBOT13 <botnk21@gmail.com>
2025-08-25 19:47:23 +0530

    Update 0sub.js

--- Priyansh/commands/0sub.js
+++ Priyansh/commands/0sub.js
@@ -12,13 +12,13 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Priyansh rajput")==0 || event.body.indexOf("sub")==0 || event.body.indexOf("subscribe")==0 || event.body.indexOf("Priyansh")==0) {
+	if (event.body.indexOf("admin")==0 || event.body.indexOf("ADMIN")==0 || event.body.indexOf("Bot banana dikha do ")==0 || event.body.indexOf("Admin")==0) {
 		var msg = {
-				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇",
+				body: "👋 𝑭𝑶𝑹 𝑨𝑵𝒀 𝑲𝑰𝑵𝑫 𝑶𝑭 𝑯𝑬𝑳𝑷 𝑪𝑶𝑵𝑻𝑨𝑪𝑻 𝑶𝑵 \n\n _______________________\n\n https://chat.whatsapp.com/IlprS6RYsjaLuBG0KgO9f2?mode=ems_share_t \n\n_______________________",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🔔", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🌹", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
